6faaa4a7446986a448cb78671dbea7372355fc76
Jane Adams <jane@universalities.com>
2019-02-22 14:23:06 -0500

    Specifying db collection

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ var password = process.env.PASSWORD;
 
 console.log('username = '+ username + ' password = ' + password)
 
-var dbconn = 'mongodb://' + username + ':' + password + '@127.0.0.1:27017/1-grams';
+var dbconn = 'mongodb://' + username + ':' + password + '@127.0.0.1:27017/1-grams.tweets';
 
 var mongoose = require('mongoose');
 
