5b819388578b02c44eb06cea085e57b7b50164d1
Jane Adams <jane@universalities.com>
2019-02-22 14:34:23 -0500

    Specifying collection in schema

--- app/models/onegram.js
+++ app/models/onegram.js
@@ -3,6 +3,10 @@ var Schema       = mongoose.Schema;
 
 var OnegramSchema   = new Schema({
     name: String
+},
+    {
+    collection: 'tweets'
+    
 });
 
 module.exports = mongoose.model('Onegram', OnegramSchema);
