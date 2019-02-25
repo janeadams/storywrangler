0bbfd66437578783315c51812b8494df8975262e
Jane Adams <jane@universalities.com>
2019-02-25 11:04:29 -0500

    Fixing HTML syntax err

--- client/src/App.js
+++ client/src/App.js
@@ -46,57 +46,6 @@ class App extends Component {
       .then(res => this.setState({ data: res.data }));
   };
 
-  // our put method that uses our backend api
-  // to create new query into our data base
-  putDataToDB = message => {
-    let currentIds = this.state.data.map(data => data.id);
-    let idToBeAdded = 0;
-    while (currentIds.includes(idToBeAdded)) {
-      ++idToBeAdded;
-    }
-
-    axios.post("http://localhost:3001/api/putData", {
-      id: idToBeAdded,
-      message: message
-    });
-  };
-
-
-  // our delete method that uses our backend api 
-  // to remove existing database information
-  deleteFromDB = idTodelete => {
-    let objIdToDelete = null;
-    this.state.data.forEach(dat => {
-      if (dat.id == idTodelete) {
-        objIdToDelete = dat._id;
-      }
-    });
-
-    axios.delete("http://localhost:3001/api/deleteData", {
-      data: {
-        id: objIdToDelete
-      }
-    });
-  };
-
-
-  // our update method that uses our backend api
-  // to overwrite existing data base information
-  updateDB = (idToUpdate, updateToApply) => {
-    let objIdToUpdate = null;
-    this.state.data.forEach(dat => {
-      if (dat.id == idToUpdate) {
-        objIdToUpdate = dat._id;
-      }
-    });
-
-    axios.post("http://localhost:3001/api/updateData", {
-      id: objIdToUpdate,
-      update: { message: updateToApply }
-    });
-  };
-
-
   // here is our UI
   // it is easy to understand their functions when you 
   // see them render into our screen
@@ -115,6 +64,7 @@ class App extends Component {
                 </li>
               ))}
         </ul>
+    </div>
     );
   }
 }
