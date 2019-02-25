b78dfe64a3611a695120da7fff484574a81e8941
Jane Adams <jane@universalities.com>
2019-02-25 14:31:01 -0500

    Debugging query

--- client/src/App.js
+++ client/src/App.js
@@ -50,7 +50,7 @@ class App extends Component {
                      return data.json();})*/
       //.then(data => data.json())
       .then(function(data){console.log("data = ", data.json());})
-      //.then(res => this.setState({ data: res.data }))
+      .setState({ data: data })
       //.then(function(res){console.log(res);})
   };
 
