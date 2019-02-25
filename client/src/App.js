e71f59b79da05e299bfbd834cd14dcde07e1cc55
Jane Adams <jane@universalities.com>
2019-02-25 14:56:55 -0500

    Debugging render

--- client/src/App.js
+++ client/src/App.js
@@ -46,10 +46,9 @@ class App extends Component {
   // fetch data from our data base
   getDataFromDb = () => {
     fetch("http://localhost:3001/api/onegrams/christmas")
-      /*.then(data => {console.log(data.body);
-                     return data.json();})*/
+      .then(data => {console.log(data.body);
+                     return data.json();})
       //.then(data => data.json())
-      .then(function(data){console.log("data.json() = ", data.json());})
       //.then(data => data.json)
       //.then(function(res){console.log(res);})
   };
