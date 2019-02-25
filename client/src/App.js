57b39bd193e644cd8a7133a0d40a417e9aad2de0
Jane Adams <jane@universalities.com>
2019-02-25 14:03:39 -0500

    Debugging fetch

--- client/src/App.js
+++ client/src/App.js
@@ -46,10 +46,10 @@ class App extends Component {
     fetch("http://localhost:3001/api/onegrams/christmas")
       /*.then(data => {console.log(data.body);
                      return data.json();})*/
-      //.then(data => data.json())
-      //.then(function(data){console.log(data);})
+      .then(data => data.json())
+      .then(function(data){console.log(data);})
       .then(res => this.setState({ data: res.data }))
-      .then(function(res){console.log(res);})
+      //.then(function(res){console.log(res);})
   };
 
   // here is our UI
