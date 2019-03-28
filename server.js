56c62df63ca1a524d160c7ed2d5fbcd5b46ec403
Jane Adams <jane@universalities.com>
2019-03-28 12:33:03 -0400

    Changing port specification for React app

--- server.js
+++ server.js
@@ -42,7 +42,7 @@ app.get('/onegrams/:word', function (req, res, next) {
   console.log('CORS-enabled web server listening on port 80')
 });*/
 
-var port = process.env.PORT || 443;        // set our port
+var port = process.env.PORT || 3001;        // set our port
 
 var Onegram = require('./app/models/onegram');
 
