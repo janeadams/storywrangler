90e5c7375725fd7310f3397e5ff1eb9b8ab724e5
Jane Adams <jane@universalities.com>
2019-07-09 13:27:23 -0400

    Fiddling w semiotic

--- semiotic/xyframe.js
+++ semiotic/xyframe.js
@@ -3,29 +3,25 @@ const theme = ["#ac58e5","#E0488B","#9fd0cb","#e0d33a","#7566ff","#533f82","#7a2
 
 var val = "christmas"
 var url = "http://hydra.uvm.edu:3001/api/onegrams/"+val
+console.log(url)
 var worddata = JSON.parse(url)
 
 const frameProps = {   lines: worddata,
   size: [700,400],
   margin: { left: 80, bottom: 90, right: 10, top: 40 },
-  xAccessor: "week",
-  yAccessor: "theaterCount",
+  xAccessor: "time",
+  yAccessor: "rank",
   yExtent: [0],
   lineStyle: (d, i) => ({
-    stroke: theme[i],
+    //stroke: theme[i],
+    stroke: "#ac58e5",
     strokeWidth: 2,
     fill: "none"
   }),
-  title: (
-    <text textAnchor="middle">
-      Theaters showing <tspan fill={"#ac58e5"}>Ex Machina</tspan> vs{" "}
-      <tspan fill={"#E0488B"}>Far from the Madding Crowd</tspan>
-    </text>
-  ),
-  axes: [{ orient: "left", label: "Number of Theaters", tickFormat: function(e){return e/1e3+"k"} },
-    { orient: "bottom", label: { name: "Weeks from Opening Day", locationDistance: 55 } }]
+  axes: [{ orient: "left", label: "Rank", tickFormat: function(e){return e/1e3+"k"} },
+    { orient: "bottom", label: { name: "Date", locationDistance: 55 } }]
 }
 
 export default () => {
-  return <XYFrame {...frameProps} />
+  return <XYFrame {frameProps} />
 }
