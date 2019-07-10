374e42eca53b536883953b6aee6be45d0b3452cd
Jane Adams <jane@universalities.com>
2019-07-10 13:45:35 -0400

    Integrating semiotic into working react bundle

--- src/index.jsx
+++ src/index.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import HelloWorld from './components/HelloWorld';
+import XYFrame from './components/xyframe';
 import './styles/app.scss'
 ReactDOM.render(
-   <HelloWorld />,
+   <XYFrame />,
    document.getElementById('app')
 );
