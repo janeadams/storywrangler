ef18f1438f5202ff6c135ca4cf07d01a4e43e1e0
Jane Adams <jane@universalities.com>
2019-07-10 13:46:21 -0400

    Integrating semiotic into working react bundle

---
+++ src/components/xyframe.jsx
@@ -0,0 +1,26 @@
+import react from "React";
+import reactDOM from "ReactDOM";
+const { XYFrame } = Semiotic;
+const theme = ["#ac58e5","#E0488B","#9fd0cb","#e0d33a","#7566ff","#533f82","#7a255d","#365350","#a19a11","#3f4482"]
+// Enable cross-origin request service
+var cors = require('cors');
+var val = "christmas";
+var url = `d3demo/json/${val}`;
+console.log(url)
+var worddata = JSON.parse(url)
+
+export default class lineChart extends React.Component {
+render() {
+return (<div>
+<h1>Line Chart</h1>
+<XYFrame
+  size={[ 700,500 ]}
+  data={worddata}
+  xAccessor={"date"}
+  yAccessor={"rank"}
+  style={d => ({ fill: d.color })}
+  margin={{ top: 30, bottom: 0, left: 80, right: 50 }}
+  />
+</div>)
+  } // End of the render function
+} // End of the component
