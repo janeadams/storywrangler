a018affe569722bd7998ff18f53dec66d787e800
Jane Adams <jane@universalities.com>
2019-07-10 11:13:46 -0400

    Configuring webpack/babel for jsx -> js back-compatibility

---
+++ src/index.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import HelloWorld from './components/HelloWorld';
+import './styles/app.scss';
+ReactDOM.render(
+   <HelloWorld />,
+   document.getElementById('app')
+);
---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+// This library allows us to combine paths easily
+const path = require('path');
+module.exports = {
+   entry: path.resolve(__dirname, 'src', 'index.jsx'),
+   output: {
+      path: path.resolve(__dirname, 'output'),
+      filename: 'bundle.js'
+   },
+   resolve: {
+      extensions: ['.js', '.jsx']
+   },
+   module: {
+      rules: [
+         {
+             test: /\.jsx/,
+             use: {
+                loader: 'babel-loader',
+                options: { presets: ['react', 'es2015'] }
+             }
+         },
+         {
+            test: /\.scss/,
+            use: ['style-loader', 'css-loader', 'sass-loader']
+         }
+      ]
+   }
+};
