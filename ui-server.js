cd0a178ea4f32dab683750973e057daf33254ded
Jane Adams <jane@universalities.com>
2019-08-12 17:46:19 -0400

    Fixing archive; getting another demo to work with local json objects

--- ui-server.js
+++ ui-server.js
@@ -8,33 +8,14 @@ var express = require('express');        // call express
 var app = express();                 // define our app using express
 var bodyParser = require('body-parser');
 
+var ui_root = __dirname + "/ui";
+console.log("Application root = ",ui_root);
 var cors = require('cors');
-
-require('dotenv').config();
-
-var username = process.env.USERNAME;
-
-var password = process.env.PASSWORD;
-
-console.log('username = '+ username + ' password = ' + password)
-
-var dbconn = 'mongodb://' + username + ':' + password + '@127.0.0.1:27017/1-grams';
-
-var mongoose = require('mongoose');
-
-
-mongoose.connect(dbconn, { useNewUrlParser: true });
-
-
-// configure app to use bodyParser()
-// this will let us get the data from a POST
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
 
 //Cross-origin request service
 app.use(cors());
 
-app.get('/onegrams/:word', function (req, res, next) {
+app.get(':word', function (req, res, next) {
   res.json({msg: 'This is CORS-enabled for all origins!'})
 });
 
@@ -42,9 +23,7 @@ app.get('/onegrams/:word', function (req, res, next) {
   console.log('CORS-enabled web server listening on port 80')
 });*/
 
-var port = process.env.PORT || 3000;        // set our port
-
-var Onegram = require('./app/models/onegram');
+var port = process.env.PORT || 8051;        // set our port
 
 
 // ROUTES FOR OUR API
@@ -58,38 +37,10 @@ router.use(function(req, res, next) {
     next(); // make sure we go to the next routes and don't stop here
 });
 
-// test route to make sure everything is working (accessed at GET http://localhost:3001/api)
-router.get('/', function(req, res) {
-    res.json({ message: 'hooray! welcome to our ui!' });   
-});
-
-router.route('/onegrams/:word')
-
-    // get the onegram for that word (accessed at GET http://localhost:{PORT}/api/onegrams/onegram_id)
-    .get(function(req, res) {
-        lines = Onegram.find({word: req.params.word}, function(err, onegram) {
-            json(onegram);});
-        const frameProps = {   lines: worddata,
-  size: [700,400],
-  margin: { left: 80, bottom: 90, right: 10, top: 40 },
-  xAccessor: "time",
-  yAccessor: "rank",
-  yExtent: [0],
-  lineStyle: (d, i) => ({
-    stroke: theme[i],
-    strokeWidth: 2,
-    fill: "none"
-  }),
-  axes: [{ orient: "left", label: "Rank", tickFormat: function(e){return e/1e3+"k"} },
-    { orient: "bottom", label: { name: "Date", locationDistance: 55 } }]
-}
-
-res.send(<XYFrame {frameProps}/>)
-    });
 
 // REGISTER OUR ROUTES -------------------------------
 // all of our routes will be prefixed with /ui
-app.use('/ui', router);
+app.use(express.static(ui_root));
 
 // START THE SERVER
 // =============================================================================
--- ui/script.js
+++ ui/script.js
@@ -1,8 +1,6 @@
-d3.json('https://dev.universalities.com/onegrams/json_from_api/halloween.json', function(error, data){
-	if(error){
-		console.log(error);
-	}
-
-	d3.select('h2#data-title').text("Halloween");
-	d3.select('div#data pre').html(JSON.stringify(data));
+d3.json("data/halloween.json").then(function(data, error) {
+	console.log('read');
+       if (error) 
+           return console.log(error);
+    d3.select('div#data').html(JSON.stringify(data.ranks));
 });
