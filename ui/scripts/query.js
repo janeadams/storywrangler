e8de75ee62c0601b17318da48062bc65d45f893a
Jane Adams <jane@universalities.com>
2019-08-21 15:14:59 -0400

    Refactoring query code, developing d3 chart

--- ui/scripts/query.js
+++ ui/scripts/query.js
@@ -1,90 +1,84 @@
 // When the form is submitted...
 function inputClick(event) {
-    // Add the query to the queries list, and import the data
-    addQuery(d3.select("#queryInput").property("value"))
-    console.log("queries = [" + queries + "]")
+    query = d3.select("#queryInput").property("value")
+    if (query == '') {
+        console.log("Nothing entered in the search box");
+    } else {
+        // If the query isn't already in our list
+        if (queries.includes(query) == false) {
+            // If we have less than 10 queries
+            if (queries.length > 10) {
+                dumpFirst()
+            }
+            // Add the query to the queries list
+            addQuery(query)
+            // Import the data and add to the list of query data objects
+            loadData(query)
+        }
+        // If the query was already in our list...
+        else {
+            console.log(query + " was already in queries array; queries = [" + queries + "]");
+        }
+    }
     // Clear the search box
     document.getElementById('queryForm').reset()
-    // Don't reload the page on submit
+    // Draw our timeseries!
     drawTimeseries()
+    // Don't reload the page on submit
     return false;
+}
+
+function dumpFirst() {
+    console.log("Maximum of 10 queries allowed!")
+    // Remove the li that corresponds to the first query
+    d3.select("#" + queries[0]).remove()
+    // Remove the first item from our query list, and remove its data from our data list
+    removeWord(queries[0], queries)
+    removeWord(queries[0], querydata)
 }
 
 // When a word is submitted via inputClick...
 function addQuery(val, err) {
-    try {
-        if (val == '') {
-            console.log("Nothing entered in the search box");
-        } else {
-            // If the query isn't already in our list
-            if (queries.includes(val) == false) {
-                // If we have less than 10 queries
-                if (queries.length > 10) {
-                    console.log("Maximum of 10 queries allowed!")
-                    // Remove the li that corresponds to the first query
-                    d3.select("#" + queries[0]).remove()
-                    // Remove the first item from our query list, and remove its data from our data list
-                    removeQuery(queries[0])
-                }
-                // Add the word to the list of queries
-                queries.push(val);
-                console.log("Added " + val + " to query list [" + queries + "]; query list length = " + queries.length);
-                // Add the word as a list item so the user knows it's been added and can delete later
-                d3.select("#queryList").append("li").text(val).attr("id", val).on("click", function(d, i) {
-                    // When the list item is clicked, remove the word from the query list and delete the data
-                    removeQuery(this.id)
-                    // Delete the li for the deleted word
-                    this.remove()
-                })
-                // When a new word is queried...
-                function loadData(word) {
-                    try {
-                        // Pull the JSON data
-                        d3.json("data/" + word + ".json").then(function(data, error) {
-                            console.log('read');
-                            // Add the JSON data object to the array of query data
-                            querydata.push(data);
-                            console.log("Added data for " + word + " to data list; querydata list length = " + querydata.length)
-                        });
-                    } catch (e) {
-                        // Error handling
-                        console.log(e);
-                        alert("Couldn't find data for " + word + "!")
-                    }
-                }
-                // Add the data to the list of query data objects
-                loadData(val)
-            }
-            // If the query was already in our list...
-            else {
-                console.log(val + " was already in queries array; queries = [" + queries + "]");
-            }
-        }
+    // Add the word to the list of queries
+    queries.push(val);
+    console.log("Added " + val + " to query list [" + queries + "]; query list length = " + queries.length);
+    // Add the word as a list item so the user knows it's been added and can delete later
+    d3.select("#queryList").append("li").text(val).attr("id", val).on("click", function(d, i) {
+        // When the list item is clicked, remove the word from the query list and delete the data
+        removeQuery(this.id)
+        // Delete the li for the deleted word
+        this.remove()
+    })
+    // Draw the timeseries
+}
 
-        // Error handling:
+// When a new word is queried...
+function loadData(word) {
+    try {
+        // Pull the JSON data
+        d3.json("data/" + word + ".json").then(function(data, error) {
+            console.log('read');
+            // Parse the dates into d3 date format
+            var parsedDates = data["dates"].map(function(date) { return d3.timeParse(date) })
+            // Set the querydata dates to the parsed dates
+            data["dates"] = parsedDates
+            // Add the JSON data object to the array of query data
+            querydata.push(data);
+            console.log("Added data for " + word + " to data list; querydata list length = " + querydata.length)
+        });
     } catch (e) {
+        // Error handling
         console.log(e);
+        alert("Couldn't find data for " + word + "!")
     }
 }
 
 // When the list item is clicked for a particular word...
-function removeQuery(value) {
-    // Delete the word from the list of queries
-    queries = queries.filter(function(ele) {
-        // Set the queries list to every query except this one
+function removeWord(value, set) {
+    // Delete the word from the [query, querydata, etc.]
+    set = set.filter(function(ele) {
+        // Filter the set to include every query except this one
         return ele != value
     })
-    console.log("removed ", value, " from query list | queries.length = " + queries.length + " queries = [" + queries + "]")
-    // Remove the data from the querydata list of objects
-    querydata = querydata.filter(function(ele) {
-        return ele.word != value
-    })
-    console.log("removed ", value, " from data array | querydata.length = " + querydata.length)
-    /* Debugging:
-    for (var i = 0; i < querydata.length; i++) {
-        var currentquery = querydata[i]
-        console.log(currentquery)
-        console.log("i = " + i + " word = " + currentquery.word)
-    }
-    */
+    console.log("removed ", value, " from set; length = " + set.length + " and data is " + set)
 }
--- ui/scripts/timeseries.js
+++ ui/scripts/timeseries.js
@@ -1,11 +1,7 @@
 function drawTimeseries() {
-    querydata.forEach(function(data) {
-        var times = []
-        data["times"].forEach(function(date) {
-            times.append(d3.time.format("%Y-%m-%d").parse(date))
-        })
-        console.log("dates = ", times)
-        var values = data[params['metric']]
-        console.log("values = ", values)
+    querydata.forEach(function(dataset) {
+        var values = dataset[params['metric']]
+        var svgcontainer = d3.select("#timeseries").append('svg').attr("width", 400).attr('height', 200)
+        var points = svgcontainer.selectAll("circle").data(values / 100).enter().append('circle');
     })
 }
