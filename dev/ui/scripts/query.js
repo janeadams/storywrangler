1a23f59f8bee40069502cfcc3647af5a07c0eb61
Jane Adams <jane@universalities.com>
2020-03-31 17:38:43 -0400

    continuing to refactor; fixing bad practice of naming classes by ngram

--- dev/ui/scripts/query.js
+++ dev/ui/scripts/query.js
@@ -4,57 +4,42 @@ function querySubmission(event) {
     if (query === '') {
         console.log("Nothing entered in the search box");
     } else {
-        // If the query isn't already in our list
-        if (params["ngrams"].includes(ngram) === false) {
-            // If this will bring us to ten queries, dump the first item in our list
-            if (params["queries"].length > 9) {
-                dumpFirst()
-            }
-            // Add the word to the list of queries
-            params["ngrams"].push(ngram);
-            console.log("Added " + ngram + " to ngram list [" + params["ngrams"] + "]; ngram list length = " + params["ngrams"].length);
-            updateURL();
-        }
-        // If the ngram was already in our list...
-        else {
-            console.log(ngram + " was already in ngrams array; params['ngrams'] = [" + params["ngrams"] + "]");
-        }
-    }
+        if (params['ngrams'].length > 9) { dumpFirst() }
+        loadData(query)
     // Clear the search box
     document.getElementById('queryForm').reset();
     // Don't reload the page on submit
-    return false;
+    return false
 }
 
 function dumpFirst() {
-    console.log("Maximum of 10 ngram searches allowed!");
-    // Remove the li that corresponds to the first ngram
-    d3.select("#" + params["ngrams"][0]).remove();
-    // Remove the first item from our ngram list, and remove its data from our data list
-    removeNgram(params["ngrams"][0])
+    console.log("Maximum of 10 searches allowed!")
+    removeNgram(params['ngrams'][0])
 }
 
 // When a word is submitted via inputClick...
-function addNgram(val, colorid, err) {
+function addNgram(value, identifier, err) {
     // Add the word as a list item so the user knows it's been added and can delete later
     d3.select("#ngramList").append("li")
-        .text(val)
-        .attr("class", "li-" + val)
-        .style("color", colors.dark[colorid])
-        .style("border-color", colors.main[colorid])
-        .style("background-color", colors.light[colorid])
+        .text(value)
+        .attr("class", "li-" + identifier)
+        .style("color", colors.dark[identifier])
+        .style("border-color", colors.main[identifier])
+        .style("background-color", colors.light[identifier])
         .on("click", function(d, i) {
             // When the list item is clicked, remove the word from the ngram list and delete the data
-            n = this.className.replace("li-", "");
-            removeNgram(n);
+            n = this.text
+            ID = this.className.replace("li-", "")
+            removeNgram(n, ID)
             // Delete the li for the deleted word
             this.remove()
         });
-    console.log("Added " + val + " to UI buttons");
+    console.log("Added " + value + " to UI buttons");
 }
 
 // When the list item is clicked for a particular word...
-function removeNgram(value) {
+function removeNgram(value, identifier) {
+    d3.select("#" + identifier).remove()
     // Delete the word from the list of queries
     params["ngrams"] = params["ngrams"].filter(ele =>
         // Filter the set to include every ngram except this one
@@ -67,9 +52,6 @@ function removeNgram(value) {
         ele['ngram'] !== value
     );
     console.log("removed ", value, " from ngramdata; length = " + ngramdata.length + " and data is " + ngramdata);
-    // Clear the chart
-    d3.select("#timeseries").selectAll().remove();
-    drawCharts()
 }
 
 function filterSubmission() {
