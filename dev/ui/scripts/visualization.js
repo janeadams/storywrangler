2afb519535cc128157706a1ebae3bedc091fee41
Jane Adams <jane@universalities.com>
2020-04-25 12:54:43 -0400

    argh

--- dev/ui/scripts/visualization.js
+++ dev/ui/scripts/visualization.js
@@ -95,7 +95,7 @@ class Chart {
         const uuid = ngramData[ngram]['uuid']
 
         const line = d3.line()
-            .x(d => this.xScale(d[0]))
+            .x(d => this.xScale(dateParser(d[0])))
             .y(d => this.yScale(d[1]))
 
         this.clipgroup.append('path')
