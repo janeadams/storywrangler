f46f87a46794fe4ab0be1a9a6f24e5ffe8909f2e
Jane Adams <jane@universalities.com>
2020-04-25 13:32:23 -0400

    labeling

--- dev/ui/scripts/load-data.js
+++ dev/ui/scripts/load-data.js
@@ -47,8 +47,6 @@ function removeNgram(n) {
     let uuid = ngramData[n]['uuid']
     console.log(`removing all elements with uuid ${uuid}`)
     d3.selectAll('.uuid-'+uuid).remove()
-    mainChart.removeLine(n)
-    subplots.forEach(subplot => subplot.removeLine(n))
     // Filter the ngram list to include every ngram except this one
     params["ngrams"] = params["ngrams"].filter(ele => ele !== n)
     // Delete the word from the list of ngram data
