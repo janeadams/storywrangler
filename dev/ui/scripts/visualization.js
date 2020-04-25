b797a0bc892df0ba659bb0cc6f081a4eade28b22
Jane Adams <jane@universalities.com>
2020-04-25 13:13:49 -0400

    argh

--- dev/ui/scripts/visualization.js
+++ dev/ui/scripts/visualization.js
@@ -7,32 +7,26 @@ class Chart {
     }
 
     createScales() {
-        const m = this.margin
-        this.xScale = d3.scaleTime().domain([dateParser(params.xrange[0]), dateParser(params.xrange[1])]).range([0, this.width-m.left])
+        this.xScale = d3.scaleTime().domain([dateParser(params.xrange[0]), dateParser(params.xrange[1])]).range([0, this.width-this.margin.left])
         console.log(`createScales( set xScale to ${this.xScale})`)
         // Choose and set time scales (logarithmic or linear) for the main plot *and* the viewfinder
         if (params["scale"] === "log") {
-            this.yViewFinderScale = d3.scaleLog().domain(params["yrange"])
             this.yScale = d3.scaleLog().domain(params["yrange"])}
         else {
-            this.yViewFinderScale = d3.scaleLinear().domain(params["yrange"])
             this.yScale = d3.scaleLinear().domain(params["yrange"])
         }
         // When showing ranks, put rank #1 at the top
         // When showing any other metric, put the highest number at the top and start at 0
         if (params["metric"] === "rank") {
-            this.yScale.range([this.height-(m.top+m.bottom), 1])
-            this.yViewFinderScale.range([this.viewFinderHeight, 1])
+            this.yScale.range([this.height-(this.margin.top+this.margin.bottom), 1])
         }
         else {
-            this.yScale.range([0, this.height-(m.top+m.bottom)])
-            this.yViewFinderScale.range([0, this.viewFinderHeight])
+            this.yScale.range([0, this.height-(this.margin.top+this.margin.bottom)])
         }
     }
 
     addAxes() {
         const height = this.height
-        const viewFinderHeight = this.viewFinderHeight
         const m = this.margin
 
         const xAxis = d3.axisBottom()
@@ -107,22 +101,19 @@ class Chart {
 
     removeLine(ngram){
         this.plot.select('.uuid-'+ngramData[ngram]['uuid']).remove()
-        this.viewfinder.select('.uuid-'+ngramData[ngram]['uuid']).remove()
     }
 
     draw() {
         this.width = this.element.offsetWidth
         this.height = this.element.offsetHeight
-        this.viewFinderHeight = 100
         this.margin = { top: 0.1 * this.height, right: 0.1 * this.width, bottom: 0.1 * this.height, left: 0.25 * this.width }
-        this.plotHeight = this.height - (this.margin.top + this.margin.bottom)
         this.createScales()
         // set up parent element and SVG
         this.element.innerHTML = ''
 
         this.svg = d3.select(this.element).append('svg')
-        this.svg.attr('width', this.width)
-        this.svg.attr('height', this.margin.top + this.height + this.margin.bottom + this.viewFinderHeight)
+        this.svg.attr('width', this.width - (this.margin.left + this.margin.right))
+        this.svg.attr('height', this.height - (this.margin.top + this.margin.bottom))
 
         this.clip = this.svg.append("defs").append("svg:clipPath")
             .attr("id", "clip")
