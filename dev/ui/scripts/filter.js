837dbe08a6c927c02cf63c7f7c20b87b853356ed
Jane Adams <jane@universalities.com>
2020-04-27 18:33:41 -0400

    filter functionality

--- dev/ui/scripts/filter.js
+++ dev/ui/scripts/filter.js
@@ -2,14 +2,15 @@ d3.selectAll("input[value ='RT']").property('checked', params['RT'])
 
 document.querySelectorAll('.filter').forEach(item => {
     item.addEventListener("click", function(e, i) {
-    // When the list item is clicked, remove the word from the query list and delete the data
-    if (this.checked == false) {
-        params[this.name] = this.value
-        this.setAttribute("checked", "checked")
-        this.checked = true
-    } else {
-        return false
-    }
+        console.log(`Clicked on ${this}`)
+        // When the list item is clicked, remove the word from the query list and delete the data
+        if (this.checked == false) {
+            params[this.name] = this.value
+            this.setAttribute("checked", "checked")
+            this.checked = true
+        } else {
+            return false
+        }
     })
 })
 
@@ -17,12 +18,12 @@ function setFilters() {
 
     // Check the boxes based on the parameters
     for (var filter of ['metric', 'language', 'scale']) {
-        console.log("Clearing all checkboxes for ", filter)
+        console.log(`Clearing all checkboxes for  ${filter}`)
         // Clear all checked boxes
-        d3.selectAll("input[name = " + filter + "]").property('checked', false)
+        d3.selectAll(`input[name = ${filter}]`).property('checked', false)
         // Check only the correct box for this parameter value
-        console.log("Checking box for", params[filter], "on filter", filter)
-        d3.selectAll("input[value = " + params[filter] + "]").property('checked', true)
+        console.log(`Checking box for ${params[filter]} on filter ${filter}`)
+        d3.selectAll(`input[value =${params[filter]}]`).property('checked', true)
     }
 
 
