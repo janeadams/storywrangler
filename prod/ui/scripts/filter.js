6f9e3ab22d79a5e70e237dc7ab796890fc3c22d7
Jane Adams <jane@universalities.com>
2020-04-27 10:56:43 -0400

    adding back in other metric filter selections

--- prod/ui/scripts/filter.js
+++ prod/ui/scripts/filter.js
@@ -1,11 +1,10 @@
 d3.select("#filterForm").select("li").addEventListener("click", function(e, i) {
     // When the list item is clicked, remove the word from the query list and delete the data
-    if (this.checked == false) {
-        currentParams[this.name] = this.value
+    if (this.checked === false) {
+        params[this.name] = this.value
         this.setAttribute("checked", "checked")
         this.checked = true
     } else {
         return false
     }
-
 })
