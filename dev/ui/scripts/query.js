467c6e4f182af62f9a36788c62b57e31bc9c9809
Jane Adams <jane@universalities.com>
2020-04-27 17:37:33 -0400

    adding back in other metric filter selections

--- dev/ui/scripts/query.js
+++ dev/ui/scripts/query.js
@@ -14,10 +14,10 @@ function querySubmission(event) {
 function filterSubmission() {
     console.log("filter selected!");
     // Check the boxes based on the parameters
-    /*for (var p of ['metric', 'scale']) {
+    for (var p of ['metric', 'scale']) {
         // Get the selected language and metric, and update the parameters variable
         params[p] = d3.select("input[name = '" + p + "']").property('value')
-    }*/
+    }
     params['RT']=d3.select("input[value ='RT']").property('checked');
     console.log("params['RT'] = ",params['RT'])
     updateURL()
