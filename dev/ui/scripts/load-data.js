2c2725feb90d293dad617eece7d0fb7567d8d725
Jane Adams <jane@universalities.com>
2020-05-01 16:28:14 -0400

    url setting

--- dev/ui/scripts/load-data.js
+++ dev/ui/scripts/load-data.js
@@ -38,7 +38,7 @@ function loadData(query) {
         })
         if (newNgrams.length > 0) {
             setRanges()
-            updateURL()
+            //updateURL()
         }
     })
 }
@@ -65,7 +65,7 @@ function removeNgram(n) {
     delete ngramData[n]
     //console.log(`removed ${n} from ngramData; length = ${Object.keys(ngramData).length} and remaining ngrams are ${Object.keys(ngramData)}`)
     setRanges()
-    updateURL()
+    //updateURL()
 }
 
 // When a word is submitted via inputClick...
