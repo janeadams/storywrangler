39ed1904fc9a7f969ddeabac37c02d80465a59dc
Jane Adams <jane@universalities.com>
2020-05-07 17:25:38 -0400

    Fixing URL parse; reformatting download button; wild adventures learning about object mutability...

--- prod/ui/scripts/url-parse.js
+++ prod/ui/scripts/url-parse.js
@@ -1,109 +1,129 @@
 function readUrlVars() {
     let vars = {};
     window.location.href.replace(/[?&]+([^=&]+)=([^&]*)/gi, (m, key, value) => {
-        console.log(`4: received key = ${key} / value = ${value}`)
+        console.log(`Read URL key = ${key} / value = ${value}`)
         // Parse arrays:
         value = value.replace("[", "").replace("/]", "")
 
-        // If the parameter has a specified set of options:
-        if (Object.keys(paramoptions).includes(key)) {
-            //console.log("paramoptions includes ", key)
-            console.log(`paramoptions[${key}] = ${paramoptions[key]}`)
-            // And the value returned is incldued in those options:
-            if (key === 'rt'){
-                if (value === 'true'){value = true}
-                if (value === 'false'){value = false}
-                vars[key] = value
-                console.log(`16: set vars[${key}] to ${value}`)
-            }
-            else {
-                if (paramoptions[key].includes(value)) {
-                    // Accept the value from the url parameter
-                    console.log(`paramoptions for ${key} includes ${value}`)
-                    vars[key] = value
-                    console.log(`24: set vars[${key}] to ${value}`)
-                } else {
-                    // If the value isn't one of the allowed options, set to default
-                    console.log(`${value} is an invalid option for the ${key} parameter! Setting ${key} to default: ${defaultparams[key]}`)
-                    vars[key] = defaultparams[key]
-                    console.log(`29: set vars[${key}] to ${value}`)
-                }
-            }
-        }
-        // Set the parameter to the value from the URL
-        //console.log("Setting key:", key, " to value:", value)
         // If the parameter should be formatted as an array:
         if (key==="ngrams") {
             // Create an array
             vars[key] = []
+            console.log(`Found ${value} for ${key}`)
             let values = value.split(",")
-            console.log(`Line 36 in url parse: key ${key} / values ${values}`)
+            console.log(`Split ${value} into ${values}`)
             // Add the value to it
             values.forEach(v => {
                 if (params['ngrams'].includes(v)){`params[ngrams] already included ${v}`}
-                else {loadData(decodeURIComponent(v))}
+                else {vars[key].push(decodeURIComponent(v))}
             })
-        } else {
-            vars[key] = value
-            console.log(`45: set vars[${key}] to ${value}`)
+        }
+        else {
+            // If the parameter has a specified set of options:
+            if (Object.keys(paramoptions).includes(key)) {
+                //console.log("paramoptions includes ", key)
+                console.log(`paramoptions[${key}]:`)
+                console.log(paramoptions[key])
+                // And the value returned is incldued in those options:
+                if (key === 'rt'){
+                    if (value === 'true'){value = true}
+                    if (value === 'false'){value = false}
+                    // Set the parameter to the value from the URL
+                    vars[key] = value
+                    console.log(`vars[${key}]:`)
+                    console.log(value)
+                }
+                else {
+                    if (paramoptions[key].includes(value)) {
+                        // Accept the value from the url parameter
+                        console.log(`paramoptions for ${key} includes ${value}`)
+                        // Set the parameter to the value from the URL
+                        vars[key] = value
+                        console.log(`vars[${key}]:`)
+                        console.log(value)
+                    } else {
+                        // If the value isn't one of the allowed options, set to default value
+                        console.log(`${value} is an invalid option for the ${key} parameter!`)
+                    }
+                }
+            }
+            else {
+                let arrayVars = ['xrange','xviewrange','yrange','yviewrange']
+                if (arrayVars.includes(key)){
+                    let dateValues = value.split(",")
+                    dateValues.forEach(d => d)
+                    vars[key] = dateValues
+                }
+            }
         }
     })
     console.log(`readURLvars() returns:`)
     console.table(vars)
-    return vars;
+    return vars
 }
 // Get the parameters from the URL
 function getUrlParams() {
     if (window.location.href.indexOf('ngrams') > -1) {
-        params['ngrams'] = readUrlVars()["ngrams"]
+        params['ngrams'] = [] // Clear any existing ngrams
+        params['ngrams'].push(readUrlVars()["ngrams"]) // Add the ngrams specified in the URL
     }
     else {
-        params['ngrams'] = defaultparams['ngrams']
+        params['ngrams'] = defaultparams['ngrams'].valueOf() // Set to default keywords
     }
-    /*
     for (let p in params) {
         // If the parameter is in the URL
         if (window.location.href.indexOf(p) > -1) {
             // set the variable to the value in the url
             //console.log("Found ", p, " parameter in URL as ", urlvar)
             params[p] = readUrlVars()[p]
-            console.log("Changed params[", p, "] to ", params[p])
+            console.log(`Changed params[${p}] to ${params[p]}`)
         }
-        else {
-            // If not specified, set to default values
-            params[p] = defaultparams[p]
-            if (p === 'ngrams'){
-                params['ngrams'].forEach(n => loadData(n))
-            }
-        }
-    }*/
+    }
 }
 
 function updateURL() {
     let currentURL = String(window.location.href)
-    console.log("currentURL = ", currentURL)
+    console.log(`currentURL:`)
+    console.log(currentURL)
     let splitURL = currentURL.split("?")
-    let newURL = String(splitURL[0]) + "?ngrams=" + params['ngrams']
-    history.pushState({},null, newURL)
-    /*
-    let customparams = {};
-    for (let p of ['ngrams', 'metric', 'language', 'scale','rt']) {
-        console.log("var p = ", p);
-        console.log("params[p] = ", params[p], " defaultparams[p] = ", defaultparams[p]);
-        if (params[p] !== defaultparams[p]) {
-            customparams[p] = params[p]
+    console.log(`splitURL:`)
+    console.log(splitURL)
+    let paramlist = [];
+    for (let p of Object.keys(params)) {
+        const autoranges = ["xrange","yrange"]
+        if (autoranges.includes(p)) {
+            // Don't include x- and y-ranges in customizable parameters; this is set automatically
+            console.log(`${p} is set automatically`)
         }
         else {
+            if (params[p] !== defaultparams[p].valueOf()) { // If the parameter doesn't match the defaults
+                console.log(`params[${p}]:`)
+                console.log(params[p])
+                console.log(`defaultparams[${p}]:`)
+                console.log(defaultparams[p])
+                const dateVars = ['xrange', 'xviewrange']
+                if (dateVars.includes(p)) {
+                    let formattedDates = []
+                    defaultparams[p].forEach(date => {
+                        let formatted = dateFormatter(date)
+                        console.log(`Formatted ${date} as ${formatted}`)
+                        formattedDates.push(formatted)
+                    })
+                    paramlist.push(p + "=" + formattedDates)
+                    console.log(`Added ${p}:${formattedDates} to paramlist. Paramlist:`)
+                } else {
+                    paramlist.push(p + "=" + params[p])
+                    console.log(`Added ${p}:${params[p]} to paramlist. Paramlist:`)
+                }
+                console.log(paramlist)
+            }
         }
     }
-    console.log("customparams = ", customparams);
-    let paramlist = [];
-    for (let [p, v] of Object.entries(customparams)) {
-        paramlist.push(p + "=" + v)
+    if (paramlist.length > 0){
+        let newURL = String(splitURL[0]) + "?" + paramlist.join("&")
+        console.log("newURL:")
+        console.log(newURL)
+        history.pushState(paramlist,'', newURL)
     }
-    let newURL = String(splitURL[0]) + "?" + paramlist.join("&");
-    console.log("newURL = ", newURL);
-    history.pushState({},null, newURL)
-    */
-
 }
+
