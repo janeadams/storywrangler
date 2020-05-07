920cc67b363314db570532e159797e96727b8725
Jane Adams <jane@universalities.com>
2020-05-07 11:42:03 -0400

    url parsing and writing

--- dev/ui/scripts/setup.js
+++ dev/ui/scripts/setup.js
@@ -60,7 +60,7 @@ function setRanges() {
         params.xrange = [d3.min(xmins), d3.max(xmaxes)]
         console.log(`Setting params[xrange] to ${params.xrange}`)
         if (params['metric'] === 'rank') {params.yrange = [d3.max(ymaxes) * 1.2, 1]}
-        else {params.yrange = [0, d3.max(ymaxes) * 1.2]}
+        else {params.yrange = [0, Math.ceil(d3.max(ymaxes) * 1.2)]}
         console.log(`Setting params[yrange] to ${params.yrange}`)
     }
     else {
