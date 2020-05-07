adc2a777e20b6dcf7c598a328567f1d8476cffc0
Jane Adams <jane@universalities.com>
2020-05-07 12:12:49 -0400

    url parsing and writing

--- dev/ui/scripts/load-data.js
+++ dev/ui/scripts/load-data.js
@@ -38,7 +38,7 @@ function loadData(query) {
             xmaxes.push(thisdata['max_date'])
             ymins.push(thisdata[`min_${params.metric}`])
             ymaxes.push(thisdata[`max_${params.metric}`])
-            ngramData[n]['data'] = tuple => [dateParser(tuple[0]),tuple[1]]
+            ngramData[n]['data'] = ngramData[n]['data'].map(tuple => [dateParser(tuple[0]),tuple[1]])
         })
         newNgrams.forEach(n => {
             // If this ngram is already in the params ngrams list
