764ce24fd98df8472e7ae4536d19e07948fb7d49
Jane Adams <jane@universalities.com>
2020-07-03 12:44:57 -0400

    adding subplots

--- dev/ui/scripts/visualization.js
+++ dev/ui/scripts/visualization.js
@@ -269,7 +269,7 @@ function addDots(chart, dataKey){
 }
 
 function updateAll(){
-    let allPlots = []
+    let allPlots = new Array
     //allPlots.push(mainChart)
     (Object.values(subPlot)).forEach(sub => allPlots.push(sub))
     console.log('allPlots:')
