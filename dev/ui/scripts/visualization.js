32eedda769b6f9d6de9b08c5e233eeefe6eee62c
Jane Adams <jane@universalities.com>
2020-07-06 17:16:30 -0400

    nesting subplots in containers

--- dev/ui/scripts/visualization.js
+++ dev/ui/scripts/visualization.js
@@ -25,9 +25,9 @@ function setScales(chart){
             .domain(xRange)
             .range([0, chart.width - m.left])
     }
-    console.log('chart.xScale')
-    console.log(chart.xScale)
-    console.log(`set xScale.domain to ${chart.xScale.domain()} and range to ${chart.xScale.range()}`)
+    //console.log('chart.xScale')
+    //console.log(chart.xScale)
+    //console.log(`set xScale.domain to ${chart.xScale.domain()} and range to ${chart.xScale.range()}`)
 
     // Choose and set time scales (logarithmic or linear) for the main plot
     if (params['metric']==='rank') {
@@ -360,8 +360,24 @@ class Chart {
         //console.log(`Running setup() for chart type ${this.type} on element ${this.element}`)
         this.width = this.element.offsetWidth
         this.height = this.element.offsetHeight
-        this.navPlotHeight = 50
-        this.margin = { top: 0.1 * this.height, right: 0.1 * this.width, bottom: (0.1 * 2 * this.height) + this.navPlotHeight, left: d3.min([0.1 * 3 * this.width, 150]) }
+        if (this.type==='main') {
+            this.navPlotHeight = 50
+            this.margin = {
+                top: 0.1 * this.height,
+                right: 0.1 * this.width,
+                bottom: (0.2 * this.height) + this.navPlotHeight,
+                left: d3.min([0.3 * this.width, 150])
+            }
+        }
+        else {
+            this.navPlotHeight = 0
+            this.margin = {
+                top: 0.1 * this.height,
+                right: 0.1 * this.width,
+                bottom: (0.1 * this.height),
+                left: d3.min([0.1 * this.width, 150])
+            }
+        }
         setScales(this)
         let parent = this
         // set up parent element and SVG
