41a5bc974083367b09d7135291f8ccac43e2f979
Jane Adams <jane@universalities.com>
2020-07-09 14:04:47 -0400

    add alert message for ngram not found

--- dev/ui/scripts/main.js
+++ dev/ui/scripts/main.js
@@ -205,22 +205,22 @@ d3.select(window).on('resize', () => {
 
 function showloadingpanel(){
     //console.log('Showing loading panel...')
-    d3.select('.pageOverlay').css({ 'display': 'block' }).append('div').attr('class','loader')
+    d3.select('.pageOverlay').style({ 'display': 'block' }).append('div').attr('class','loader')
 }
 
 function hideloadingpanel(){
     //console.log('Hiding loading panel...')
-    d3.select('.pageOverlay').css({ 'display': 'none' })
+    d3.select('.pageOverlay').style({ 'display': 'none' })
     d3.selectAll('.loader').remove()
 }
 
 function showAlert(msg){
     console.log('Showing alert message')
     //hideAlert()
-    d3.select('.pageOverlay').css({ 'display': 'block' }).append('div').attr('class','alert').html('<p>'+msg+'</p>')
+    d3.select('.pageOverlay').style({ 'display': 'block' }).append('div').attr('class','alert').html('<p>'+msg+'</p>')
 }
 
 function hideAlert(){
-    d3.select('.pageOverlay').css({ 'display': 'none' })
+    d3.select('.pageOverlay').style({ 'display': 'none' })
     d3.selectAll('.alert').remove()
 }
