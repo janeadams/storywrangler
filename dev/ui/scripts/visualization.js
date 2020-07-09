ac07ca868a7ea35e3d9a7d47e65becc66e45846c
Jane Adams <jane@universalities.com>
2020-07-09 15:08:43 -0400

    round out subplot vals for freq

--- dev/ui/scripts/visualization.js
+++ dev/ui/scripts/visualization.js
@@ -494,9 +494,9 @@ function addSuplot(ngram){
     let parsed_max = `${max}`
     if (params['metric']==='freq'){ // Round out the digits of the min/max values for frequency
         //parsed_min = `~${parseFloat(precise(min,3))}`
-        parsed_min = `1 in ~${parseFloat(precise(1/min, 3))}`
+        parsed_min = `about 1 in ${parseFloat(precise(1/min, 3)).toLocaleString()}`
         //parsed_max = `~${parseFloat(precise(max,3))}`
-        parsed_max = `1 in ~${parseFloat(precise(1/max, 3))}`
+        parsed_max = `about 1 in ${parseFloat(precise(1/max, 3)).toLocaleString()}`
     }
     d3.select('#subplots').append('div').attr("class", `subplot-container ${subplotClass}`)
         .append('div').attr("class", "subplot-details")
