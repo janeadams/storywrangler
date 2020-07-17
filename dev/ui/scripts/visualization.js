497e7ea488e67e9b1260c0e2fbb87cf9218db905
Jane Adams <jane@universalities.com>
2020-07-17 11:41:11 -0400

    dont redraw all glyphs on resize

--- dev/ui/scripts/visualization.js
+++ dev/ui/scripts/visualization.js
@@ -352,8 +352,8 @@ class Chart {
             console.log('brushed! event selection:')
             console.log(d3.event.selection)
             console.log(`this xScale = ${this.xScale}`)
-            this.clipgroup.property("value", d3.event.selection.map(this.xScale.invert, this.xScale).map(d3.utcDay.round));
-            this.clipgroup.dispatch("input");
+            this.svg.property("value", d3.event.selection.map(this.xScale.invert, this.xScale).map(d3.utcDay.round));
+            this.svg.dispatch("input");
         }
         //setScales(this)
         //addAxes(this)
