9296ed87ab0a2afdd6f886a931965061d7915e78
Jane Adams <jane@universalities.com>
2020-07-17 13:13:48 -0400

    dont redraw all glyphs on resize

--- dev/ui/scripts/visualization.js
+++ dev/ui/scripts/visualization.js
@@ -352,6 +352,7 @@ class Chart {
             console.log('brushed! event selection:')
             console.log(d3.event.selection)
             this.xScale.domain([params['start'], params['end']])
+            console.log(`this.xScale.domain = ${this.xScale.domain}`)
             this.xAxisGroup.call(d3.axisBottom().scale(this.xScale)).ticks(12).selectAll("text")
                 .style("text-anchor", "end")
                 .attr("dx", "-.8em")
