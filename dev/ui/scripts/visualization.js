e94f77ab70d0bf4befafc8b66a1f2a71dd9dd738
Jane Adams <jane@universalities.com>
2020-07-27 15:55:20 -0400

    adjust height of main plot for mobile

--- dev/ui/scripts/visualization.js
+++ dev/ui/scripts/visualization.js
@@ -406,16 +406,12 @@ class Chart {
         }
         if (this.type==='main') {
             this.margin.right = 0.1 * this.width
-            if (mobileScale){
-                this.margin.left = 0.15 * this.width
-            }
+            if (mobileScale){this.margin.left = 40}
             else {this.margin.left = d3.min([0.3 * this.width, 150])}
         }
         else {
             this.margin.right = 0
-            if (mobileScale){
-                this.margin.left = 0.15 * this.width
-            }
+            if (mobileScale){this.margin.left = 30}
             else {this.margin.left = 0.2 * this.width}
         }
         setScales(this)
