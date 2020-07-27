2b40c092742e717427af92344a45e2d0b7ddbf5e
Jane Adams <jane@universalities.com>
2020-07-27 08:04:42 -0400

    removing console logs

--- dev/ui/scripts/compare/setup.js
+++ dev/ui/scripts/compare/setup.js
@@ -9,16 +9,12 @@ let defaultNgrams = []
 let defaultDict = {}
 
 function setDefaults() {
-    d3.json('language_defaults.json').then((data) => {
-        defaultDict = data
-        //console.log(defaultDict)
-        if (Object.keys(defaultDict).includes(params['language'])){
-            console.log(`Alternative defaults detected for ${params['language']}`)
-            defaultNgrams = Object.assign([], defaultDict[params['language']])
-        }
-        else {
-            defaultNgrams = Object.assign([], ["🦠","hahaha","Black Lives Matter","#MeToo"])
-        }
-        console.log(`defaultNgrams: ${defaultNgrams}`)
-    })
+    if (Object.keys(defaultDict).includes(params['language'])){
+        console.log(`Alternative defaults detected for ${params['language']}`)
+        defaultNgrams = Object.assign([], defaultDict[params['language']])
+    }
+    else {
+        defaultNgrams = Object.assign([], ["🦠","hahaha","Black Lives Matter","#MeToo"])
+    }
+    console.log(`defaultNgrams: ${defaultNgrams}`)
 }
