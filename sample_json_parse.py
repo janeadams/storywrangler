a8df74a16dba78857e83d299a84d4db52ce74406
Jane Adams <jane@universalities.com>
2020-07-28 00:12:41 -0400

    adding simple json parse

---
+++ sample_json_parse.py
@@ -0,0 +1,13 @@
+import json
+import pandas as pd
+import csv
+
+data = {}
+
+with open('storywrangler_data.json') as json_file:
+    data = json.load(json_file)
+    for ngram in data['metadata']['ngrams']:
+        dfs[ngram] = pd.DataFrame(data['data'][ngram], columns=['date',data['metadata']['metric']])
+        
+for ngram in dfs:
+    dfs[ngram].to_csv(f"{ngram}-{data['metadata']['language']}-{data['metadata']['metric']}.csv")
