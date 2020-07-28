93169f31cceac4634b4b2def11430465b7045daa
Jane Adams <jane@universalities.com>
2020-07-28 14:05:21 -0400

    add zooming

--- dev/ui/scripts/visualization.js
+++ dev/ui/scripts/visualization.js
@@ -477,7 +477,7 @@ class Chart {
                 .call(this.brush)
 
             let zoom = d3.zoom()
-                .scaleExtent([1, 10])
+                .scaleExtent([1, 15])
                 .on('zoom', function() {
                     console.log('zoomed called')
                     if (d3.event) {
