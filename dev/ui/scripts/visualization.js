e9d3f4deeab57bb2ce0f75af56f94ce863a253a8
Jane Adams <jane@universalities.com>
2020-07-27 22:22:59 -0400

    add zooming

--- dev/ui/scripts/visualization.js
+++ dev/ui/scripts/visualization.js
@@ -491,7 +491,7 @@ class Chart {
                         console.log('parent:')
                         console.log(parent)
                         let newView = t.rescaleX(d3.scaleTime().domain(xRange[0], xRange[1]))
-                        console.log(`newView: ${newView}`)
+                        console.log(`newView: [${newView[0]}, ${newView[1]}]`)
                         console.log(`params['end'] - params['start'] = ${params['end'] - params['start']}`)
                         let paramDiff = newView[1] - newView[0]
                         if (paramDiff < 5000000000){
