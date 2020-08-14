444c4f77c11e64ff8940154940012a71bf8fbc5d
Jane Adams <jane@universalities.com>
2020-08-14 16:19:38 -0400

    Adding zipf dist to api with all and max rank params

--- dev/api/orm.py
+++ dev/api/orm.py
@@ -44,6 +44,14 @@ def print_info(varlist):
         l.append(k+" = "+str(v))
     return print(" | ".join(l))
 
+def get_unique_count(date, language, ngrams):
+    db = client['languages']
+    collection = db['languages']
+    for result in collection.find({'language':language, 'time':date}):
+        unique = result[f'unique_{ngrams}']
+        print(f'Unique {ngrams} for {language} on {date}: {unique}')
+    return int(unique)
+
 def get_ngrams(language, q):
     q = r.remove_whitespaces(q)
     ngrams = list(r.ngram_parser(q, regex))
@@ -77,13 +85,19 @@ def get_ngrams(language, q):
 
 
 
-def give_instructions():
-    return "Enter a URL containing a query</br>in the format <b>/api/</b><em>&lt;query&gt;</em><b>?language=</b><em>&lt;en,es,ru,fr...&gt;</em><b>&metric=&lt;rank,freq&gt;&rt=</b>&lt;true,false&gt;</br></br>e.g. <a href='https://storywrangling.org/api/happy new year?metric=freq&rt=false' target='_blank'>https://storywrangling.org/api/<b>happy new year</b>?metric=<b>freq</b>&rt=<b>false</b></a> to get all original tweets (no retweeets)</br>frequency data for the 3-gram <em>happy new year</em> from the ngrams database.</br></br>or try <a href='https://storywrangling.org/api/bonjour?language=fr&metric=freq&rt=true' target='_blank'>https://storywrangling.org/api/<b>bonjour</b>?language=<b>fr</b>&metric=<b>freq</b>&rt=<b>true</b></a> to get all frequency data </br>(including retweeets) for the 1-gram <em>bonjour</em> from the French ngrams database.</br></br>Also... emojis are supported!</br><a href='https://storywrangling.org/api/🐙' target='_blank'>https://storywrangling.org/api/🐙</a></br></br>If we can, we'll try to return the values for a full phrase; if we don't have data for the full phrase,</br>we'll break up your query into 1-grams and return data for each 1-gram.</br>Commas and other delimeters are parsed as words too. </br></br>For a full explanation of the regex used, consult our documentation,</br>which will be added <a href='https://gitlab.com/compstorylab/storywrangler'>here</a> pending paper publication on the ArXiv.</br></br>Don't forget to check out the UI at <a href='https://storywrangling.org' target='_blank'>https://storywrangling.org</a>!"
+def give_ngram_instructions():
+    return "Enter a URL containing a query</br>in the format <b>/api/ngrams/</b><em>&lt;query&gt;</em><b>?language=</b><em>&lt;en,es,ru,fr...&gt;</em><b>&metric=&lt;rank,freq&gt;&rt=</b>&lt;true,false&gt;</br></br>e.g. <a href='https://storywrangling.org/api/ngrams/happy new year?metric=freq&rt=false' target='_blank'>https://storywrangling.org/api/ngrams/<b>happy new year</b>?metric=<b>freq</b>&rt=<b>false</b></a> to get all original tweets (no retweeets)</br>frequency data for the 3-gram <em>happy new year</em> from the ngrams database.</br></br>or try <a href='https://storywrangling.org/api/ngrams/bonjour?language=fr&metric=freq&rt=true' target='_blank'>https://storywrangling.org/api/ngrams/<b>bonjour</b>?language=<b>fr</b>&metric=<b>freq</b>&rt=<b>true</b></a> to get all frequency data </br>(including retweeets) for the 1-gram <em>bonjour</em> from the French ngrams database.</br></br>Also... emojis are supported!</br><a href='https://storywrangling.org/api/ngrams/🐙' target='_blank'>https://storywrangling.org/api/ngrams/🐙</a></br></br>If we can, we'll try to return the values for a full phrase; if we don't have data for the full phrase,</br>we'll break up your query into 1-grams and return data for each 1-gram.</br>Commas and other delimeters are parsed as words too. </br></br>For a full explanation of the regex used, consult our documentation,</br>which will be added <a href='https://gitlab.com/compstorylab/storywrangler'>here</a> pending paper publication on the ArXiv.</br></br>Don't forget to check out the UI at <a href='https://storywrangling.org' target='_blank'>https://storywrangling.org</a>!"
+
+def give_zipf_instructions():
+    return "Enter a URL containing a date (YYYY-MM-DD) query</br>in the format <b>/api/zipf/</b><em>&lt;date&gt;</em><b>?language=</b><em>&lt;en,es,ru,fr...&gt;</em></br></br>e.g. <a href='https://storywrangling.org/api/zipf/2020-03-28?language=en' target='_blank'>https://storywrangling.org/api/zipf/<b>2020-03-28</b>?language=<b>en</b></a> to get the top 1000 most-used ngrams' usage data in all English tweets on January 1, 2020</br>"
+
+@app.route('/api/zipf/', methods=['GET'])
+def zipf_response():
+    return give_zipf_instructions()
 
 
-
-@app.route('/api/', methods=['GET'])
-def simple_response():
+@app.route('/api/ngrams/', methods=['GET'])
+def ngrams_response():
     start = time.time()
     pid = uuid.uuid4()
     src='root'
@@ -98,12 +112,76 @@ def simple_response():
         write_outfile = csv.writer(fd)
         write_outfile.writerow([int(pid),float((end-start)*60),['No query; returned instructions']])
         fd.close()
-    return give_instructions()
+    return give_ngram_instructions()
+
+@app.route('/api/zipf/<query>', methods=['GET'])
+def zipf_data(query):
+    start = time.time()
+    pid = uuid.uuid4()
+    date = request.date
+    ip = request.remote_addr
+    # Pull the language from the URL params, e.g. 'en', 'es', 'ru'
+    language = str(request.args.get('language'))
+    if language in list(language_codes['db_code']):
+        language = language
+    else:
+        language = 'en'
+    ngrams = str(request.args.get('ngrams'))+"grams"
+    if ngrams in ['1grams','2grams','3grams']:
+        if language not in language_support[ngrams]:
+            ngrams = '1grams'
+    else:
+        ngrams = '1grams'
+    # Pull the date requested
+    try:
+        date = datetime.datetime.strptime(query, '%Y-%m-%d')
+    except:
+        return ("Sorry, date not formatted correctly or not included in our database. Dates should be formatted as 2020-03-28")
+    show_all = request.args.get('all') == 'true'
+    output = {'date':date,'language': language, 'ngrams':ngrams}
+    db = client[ngrams]
+    collection = db[language]
+    if show_all:
+        try:
+            unique_count = get_unique_count(date, language, ngrams)
+            output[f'unique_{ngrams}'] = unique_count
+            words = [None] * unique_count
+            data = np.zeros((unique_count,4))
+            i = 0
+            for result in collection.find({'time':date}):
+                words[i]=result['word']
+                data[i][0]=result['rank']
+                data[i][1]=result['rank_noRT']
+                data[i][2]=result['freq']
+                data[i][3]=result['freq_noRT']
+                i+=1
+            df = pd.DataFrame(data=data, columns=['rank', 'rank_noRT','freq','freq_noRT'])
+            df['ngram']=words
+            output['elapsed_time']=(time.time()-start)
+            output['data']=df.to_dict('index')
+        except:
+            output['elapsed_time']=(time.time()-start)
+            output['error'] = (f"Sorry, we had trouble returning zipf data for {date} in the {language} {ngrams} database")
+    else:
+        try:
+            max_rank = int(request.args.get('max'))
+        except:
+            max_rank = 1000
+        output['max']=max_rank
+        try:
+            df = pd.DataFrame(columns=['ngram','rank', 'rank_noRT','freq','freq_noRT'])
+            for result in collection.find({'time':date, "rank": {"$lte": max_rank}}):
+                df = df.append({'ngram': result['word'], 'rank': result['rank'], 'rank_noRT': result['rank_noRT'],'freq':result['freq'],'freq_noRT':result['freq_noRT']},ignore_index=True)
+            output['elapsed_time']=(time.time()-start)
+            output['data']=df.to_dict('index')
+        except:
+            output['elapsed_time']=(time.time()-start)
+            output['error'] = (f"Sorry, we had trouble returning zipf data for {date} in the {language} {ngrams} database")
+    return jsonify(output)
 
 
-
-@app.route('/api/<query>', methods=['GET'])
-def get_data(query):
+@app.route('/api/ngrams/<query>', methods=['GET'])
+def ngram_data(query):
     start = time.time()
     # Convert '%23' to '#', etc.
     query = urllib.parse.unquote(query)
@@ -194,5 +272,5 @@ def get_data(query):
     return jsonify(output)
 
 if __name__ == '__main__':
-    app.run(debug=True, port='3000')
+    app.run(debug=True, port='3001')
 
