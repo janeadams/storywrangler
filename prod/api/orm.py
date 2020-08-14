a57ed3c5d0e46e4c7a6ca11624f9ae4f7c369cca
Jane Adams <jane@universalities.com>
2020-08-14 10:50:22 -0400

    update api routing

--- prod/api/orm.py
+++ prod/api/orm.py
@@ -81,7 +81,7 @@ def give_ngram_instructions():
     return "Enter a URL containing a query</br>in the format <b>/api/ngrams/</b><em>&lt;query&gt;</em><b>?language=</b><em>&lt;en,es,ru,fr...&gt;</em><b>&metric=&lt;rank,freq&gt;&rt=</b>&lt;true,false&gt;</br></br>e.g. <a href='https://storywrangling.org/api/ngrams/happy new year?metric=freq&rt=false' target='_blank'>https://storywrangling.org/api/ngrams/<b>happy new year</b>?metric=<b>freq</b>&rt=<b>false</b></a> to get all original tweets (no retweeets)</br>frequency data for the 3-gram <em>happy new year</em> from the ngrams database.</br></br>or try <a href='https://storywrangling.org/api/ngrams/bonjour?language=fr&metric=freq&rt=true' target='_blank'>https://storywrangling.org/api/ngrams/<b>bonjour</b>?language=<b>fr</b>&metric=<b>freq</b>&rt=<b>true</b></a> to get all frequency data </br>(including retweeets) for the 1-gram <em>bonjour</em> from the French ngrams database.</br></br>Also... emojis are supported!</br><a href='https://storywrangling.org/api/ngrams/🐙' target='_blank'>https://storywrangling.org/api/ngrams/🐙</a></br></br>If we can, we'll try to return the values for a full phrase; if we don't have data for the full phrase,</br>we'll break up your query into 1-grams and return data for each 1-gram.</br>Commas and other delimeters are parsed as words too. </br></br>For a full explanation of the regex used, consult our documentation,</br>which will be added <a href='https://gitlab.com/compstorylab/storywrangler'>here</a> pending paper publication on the ArXiv.</br></br>Don't forget to check out the UI at <a href='https://storywrangling.org' target='_blank'>https://storywrangling.org</a>!"
 
 def give_zipf_instructions():
-    return "Enter a URL containing a date (YYYY-MM-DD) query</br>in the format <b>/api/zipf/</b><em>&lt;date&gt;</em><b>?language=</b><em>&lt;en,es,ru,fr...&gt;</em></br></br>e.g. <a href='https://storywrangling.org/api/zipf/01-01-2020?language=en' target='_blank'>https://storywrangling.org/api/zipf/<b>01-01-2020</b>?language=<b>en</b></a> to get the top 1000 most-used ngrams' usage data in all English tweets on January 1, 2020</br>"
+    return "Enter a URL containing a date (YYYY-MM-DD) query</br>in the format <b>/api/zipf/</b><em>&lt;date&gt;</em><b>?language=</b><em>&lt;en,es,ru,fr...&gt;</em></br></br>e.g. <a href='https://storywrangling.org/api/zipf/2020-03-28?language=en' target='_blank'>https://storywrangling.org/api/zipf/<b>2020-03-28</b>?language=<b>en</b></a> to get the top 1000 most-used ngrams' usage data in all English tweets on January 1, 2020</br>"
 
 @app.route('/api/zipf/', methods=['GET'])
 def zipf_response():
@@ -124,9 +124,13 @@ def zipf_data(query):
             ngrams = '1grams'
     else:
         ngrams = '1grams'
+    try:
+        max_rank = int(request.args.get('max'))
+    except:
+        max_rank = 1000
     # Pull the date requested
     try:
-        date = datetime.datetime.strptime(query, '%Y-%M-%d')
+        date = datetime.datetime.strptime(query, '%Y-%m-%d')
     except:
         return ("Sorry, date not formatted correctly or not included in our database. Dates should be formatted as 2020-01-01")
     try:
@@ -134,7 +138,7 @@ def zipf_data(query):
         db = client[ngrams]
         collection = db[language]
         df = pd.DataFrame(columns=['ngram','rank', 'rank_noRT','freq','freq_noRT'])
-        for result in collection.find({'time':date, "rank": {"$lt": 1000}}):
+        for result in collection.find({'time':date, "rank": {"$lte": max_rank}}):
             df = df.append({'ngram': result['word'], 'rank': result['rank'], 'rank_noRT': result['rank_noRT'],'freq':result['freq'],'freq_noRT':result['freq_noRT']},ignore_index=True)
         output['data']=df.to_dict()
         return jsonify(output)
