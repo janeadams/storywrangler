0901b5aa47adb9a52e145d8561116dedbcfe2f76
Rachel Dotey <rachel.dotey@gmail.com>
2022-03-28 11:44:02 -0400

    Moved repository to GitLab
    The Storywrangler repository has been moved to GitLab.
    https://gitlab.com/compstorylab/storywrangling-website

--- story-viewer/src/components/index.js
+++
@@ -1,6 +0,0 @@
-export { default as Navigation } from "./Navigation";
-export { default as Footer } from "./Footer";
-export { default as Home } from "./Home";
-export { default as View } from "./View";
-export { default as About } from "./About";
-export { default as Contact } from "./Contact";
